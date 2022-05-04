587f458aa2b2e039102a2c3198b19324ef333307
Cezary Buliszak <cezary.buliszak@gmail.com>
2022-05-04 07:56:48 +0200

    initial commit.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="welcome">
+    <h1 class="welcome__header">
+      Hi, <code>&#123;&#123; your_name &#125;&#125;</code>!
+    </h1>
+    <p class="welcome__text">Welcome to <code>cm-assignment-vue</code>.</p>
+    <p class="welcome__text">
+      There's a <RouterLink to="/recipes">recipes view</RouterLink> that needs
+      some work. See the <code>README.MD</code> file for more details.
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.welcome {
+  &__header {
+    margin: 12px 0;
+  }
+
+  &__text {
+    margin: 0 0 12px 0;
+  }
+}
+</style>
